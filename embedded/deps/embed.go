11fc7abd9e5b9237e6eb3e86036028a20653b8a2
danielerdmannarv <daniel.erdmann.bertelsmann@gmail.com>
2026-02-26 06:03:48 +0100

    Update embed.go

--- embedded/deps/embed.go
+++ embedded/deps/embed.go
@@ -8,21 +8,3 @@ func GetDependencyZIP(name string) []byte {
 	// Dependencies are fetched at runtime or via external sources
 	return nil
 }
-
-// Update GetAvailableDependencies to reflect that they are now available
-func GetAvailableDependencies() []DependencyInfo {
-	return []DependencyInfo{
-		{
-			Name:        "abapgit-standalone",
-			Description: "abapGit standalone program",
-			Package:     "$ABAPGIT",
-			Available:   false, // Not embedded - requires external source
-		},
-		{
-			Name:        "abapgit-dev",
-			Description: "abapGit developer edition",
-			Package:     "$ZGIT_DEV",
-			Available:   false, // Not embedded - requires external source
-		},
-	}
-}
